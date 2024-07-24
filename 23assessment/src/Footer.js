fefd828254d4c4e06bfe779b5cc071fac40b16d1
srivallithanga <srivallithanga173@gmail.com>
2024-07-23 21:06:00 -0700

    Delete 23assessment directory

--- 23assessment/src/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return (
-        <div className="Footer">
-            <a href="#">Policy</a>
-            <a>Copyrights @..</a>
-        </div>
-    )
-}
-export default Footer;
